86f30a2ec63b25f98b91c0190b242485edae558c
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-01-27 14:24:29 +0100

    ObjectsGame #2 - oo solution with lists, forEach rectangle

--- src/at/rangel/games/firstgame/ObjectsGame.java
+++ src/at/rangel/games/firstgame/ObjectsGame.java
@@ -2,7 +2,12 @@ package at.rangel.games.firstgame;
 
 import org.newdawn.slick.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ObjectsGame extends BasicGame {
+    private List<Rectangle> rectangles;
     private Rectangle rectangle;
     private Rectangle rectangle2;
 
@@ -12,20 +17,28 @@ public class ObjectsGame extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
+        this.rectangles = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            Rectangle rectangle = new Rectangle(random.nextInt(600), random.nextInt(600), random.nextInt(1,30));
+            this.rectangles.add(rectangle);
+        }
         this.rectangle = new Rectangle(100, 100, 5);
         this.rectangle2 = new Rectangle(0, 400, 30);
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        this.rectangle.update(delta);
-        this.rectangle2.update(delta);
+        for(Rectangle rectangle: this.rectangles) {
+            rectangle.update(delta);
+        }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        this.rectangle.render(graphics);
-        this.rectangle2.render(graphics);
+        for(Rectangle rectangle: this.rectangles) {
+           rectangle.render(graphics);
+        }
         graphics.drawString("Hello", 0, 0);
     }
 
